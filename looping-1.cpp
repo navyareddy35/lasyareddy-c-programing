75428381fd0a0354c927a778c8017663261ebe1f
navyareddy35 <navyayarrabolu3527@gmail.com>
2025-08-06 23:27:44 +0530

    Add files via upload

---
+++ looping-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n = 5, i;
+    int fact = 1;
+    for (i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    printf("Factorial of %d is %d\n", n, fact);
+    return 0;
+}
+
