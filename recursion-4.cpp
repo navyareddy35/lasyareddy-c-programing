a2aebc3804f87984a4d38260a04c9d26d0f7d2f4
navyareddy35 <navyayarrabolu3527@gmail.com>
2025-08-06 23:45:27 +0530

    Add files via upload

---
+++ recursion-4.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char str[], int start, int end) {
+    if (start >= end)
+        return;
+
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+
+    reverseString(str, start + 1, end - 1);
+}
+
+int main() {
+    char str[] = "recursion";
+    reverseString(str, 0, strlen(str) - 1);
+    printf("Reversed string: %s", str);
+    return 0;
+}
+
