6accb59e4ebd98218baaea6d68ca0906163ca259
navyareddy35 <navyayarrabolu3527@gmail.com>
2025-08-06 23:25:12 +0530

    Add files via upload

---
+++ functions-1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    int fact = 1;
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num = 5;
+    printf("Factorial = %d", factorial(num));
+    return 0;
+}
+
