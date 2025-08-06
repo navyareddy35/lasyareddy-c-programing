4861b34ffde8d113b04a0565d8083656ca5e33ed
navyareddy35 <navyayarrabolu3527@gmail.com>
2025-08-06 23:44:45 +0530

    Add files via upload

---
+++ recursion-3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sumOfDigits(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return (n % 10) + sumOfDigits(n / 10);
+}
+
+int main() {
+    int n = 1234;
+    printf("Sum of digits of %d is %d", n, sumOfDigits(n));
+    return 0;
+}
+
